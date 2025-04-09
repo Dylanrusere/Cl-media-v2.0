1023829b446ce97e05d1f589fdb1ed9f2a9ac723
dylanrusere <dylaneight01@gmail.com>
2025-04-09 13:36:43 +0200

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import img2 from "../assets/images/IMG_1698.JPG";
 import img3 from "../assets/images/IMG_1698.JPG";
 import img4 from "../assets/images/IMG_1698.JPG";
 import { Footer } from "../components/Footer";
+import { ExplorationsComponent } from "./ExplorationsComponent";
 
 const images = [
     img1,
@@ -74,50 +75,7 @@ export const Home = () => {
       <div className="spacing_div"></div>
 
       {/* Blog Section */}
-      <section id="blog">
-        <div className="overall_container">
-          <p className="blog_overall_heading">Explorations</p>
-          <div className="blog_grid_container">
-
-            <div className="blog_card_container lifestyle_container">
-              <button className="green_blog_btn">#Lifestyle</button>
-              <div className="blog_img_container">
-                <img src="./assets/images/IMG_4429.JPG" alt="" />
-              </div>
-              <p className="blog_date">July 22, 2028</p>
-              <p className="blog_heading">WFH`s the New Normal</p>
-              <a href="./article1.html" className="blog_link">
-                Tap in <i className="fa-solid fa-arrow-right-long"></i>
-              </a>
-            </div>
-
-            <div className="blog_card_container mental_health_container">
-              <button className="green_blog_btn">#Mental Health</button>
-              <div className="blog_img_container">
-                <img src="./assets/images/IMG_4429.JPG" alt="" />
-              </div>
-              <p className="blog_date">July 22, 2028</p>
-              <p className="blog_heading">What`s New with the Company</p>
-              <a href="./article2.html" className="blog_link">
-                Tap in <i className="fa-solid fa-arrow-right-long"></i>
-              </a>
-            </div>
-
-            <div className="blog_card_container art_container">
-              <button className="green_blog_btn">#Art</button>
-              <div className="blog_img_container">
-                <img src="./assets/images/IMG_4429.JPG" alt="" />
-              </div>
-              <p className="blog_date">July 22, 2028</p>
-              <p className="blog_heading">How to implement AI In Your Work</p>
-              <a href="./article3.html" className="blog_link">
-                Tap in <i className="fa-solid fa-arrow-right-long"></i>
-              </a>
-            </div>
-
-          </div>
-        </div>
-      </section>
+      <ExplorationsComponent/>
 
       {/* Footer */}
       <Footer/>
